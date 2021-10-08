39bdc930a705f02ac2e9db00e2eb4f1f927c1de0
kkjos <k19951227@gmail.com>
2021-10-08 00:32:40 +0000

    [finish]base_chapter7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -26,6 +26,12 @@ class BlogsController < ApplicationController
     blog.save
     redirect_to blogs_path(blog.id)
   end
+  
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
 
   private
   def blog_params
